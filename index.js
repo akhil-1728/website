918baa2fe299e53af6ee10c18f081c39337e06fc
jojogarrix1728 <119589625+jojogarrix1728@users.noreply.github.com>
2022-12-01 20:01:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('nav').toggleClass("fixed-top", ($(window).scrollTop() > 60));
+    });
+});
+
+window.onload = function () {
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
